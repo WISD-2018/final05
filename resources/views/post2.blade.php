4ec5b481fa76139625b12ab4cc8c5cbd35899503
3A532059 <coco214034@gmail.com>
2019-01-05 15:22:45 +0800

    post2.blade.php

---
+++ resources/views/post2.blade.php
@@ -0,0 +1,106 @@
+@extends('layouts.master')
+@section('title','土魠的季節，好吃的滋味！')
+@section('content')
+
+    <!-- Page Header -->
+    <!-- Set your background image for this header on the line below. -->
+    <header class="intro-header" style="background-image: url('img/IMAG1071.jpg')">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                    <div class="site-heading">
+                        <font color="#FFFFFF" size="12" face="標楷體"><b>今年五月，花火節正式登場！</b></font>
+                        <hr class="small">
+                        <font color="#FFFFFF" size="5" face="標楷體">帶著你的老婆們一起來到這個地方<br>跟我們享受這個美好的時光吧！</font>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <!-- Main Content -->
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12" style="background-color:transparent">
+                <div class="col-md-9">
+                    <div class="card-body" style="background-color:transparent">
+                        <font color="#000000" size="4" face="標楷體">
+                            <h2>比跨年煙火還美！澎湖花火節行程安排大攻略</h2>
+                            　　澎湖花火節是台灣離島中最盛大也最好玩的季節，身為國外旅遊雜誌中台灣必去景點第一名的澎湖，每年4月中至6月中都是花火節的季節，通常過年前比較好的時間與民宿就會被訂光光，所以差不多在1月就必須做完功課直接下訂！小編是一個愛海愛島嶼的海人，今天就來教大家前往澎湖花火節的行程攻略吧！
+                            <h3>2018 澎湖花火節時間 & 地點</h3>
+                            　　　Facebook：https://www.facebook.com/phfireworks/
+                            <br>　　　地點：馬公市觀音亭
+                            <br>　　　節目活動時間：晚上8點開始 （開幕19:30開始）
+                            <br>　　　花火施放時間：晚上9點 施放15分鐘 （開幕20分鐘）
+                            <br>　　　最新消息及異動以「澎湖縣政府旅遊處」公告為主
+                            <hr>
+                            <b>2018年4月</b> 花火節施放日期
+                            <br>4/19(四)、 4/23(一)、4/26(四)、 4/30(一)
+                            <br><b>2018年5月</b> 花火節施放日期
+                            <br>5/03(四)、5/07(一)、5/10(四)、5/12(六)、 5/14(一)、5/17(四)、 5/21(一)、5/24(四)、 5/28(一)、5/31(四)
+                            <br><b>2018年6月</b> 花火節施放日期
+                            <br>6/04(一)、6/07(四)、 6/11(一)、6/14(四)、 6/18(一)、6/21(四)
+                            <br>
+                            <br>■目前尚未公布每一場次的施放地點
+                            <br>■周一及週四於觀音亭休閒園區施放，其中5場移師至五鄉各辦理一場次，包含望安鄉、湖西鄉、七美鄉、西嶼鄉及白沙鄉
+                            <br>■自107年4月19日至6月21日(每週一及週四)及5月12日(周六)假日場次辦理，共計20場次
+                            <br><h3><b>行程要規劃！澎湖花火不是天天放</b></h3>
+                            <img src="img/post/post2-5.jpg" width="50%">
+                            <br>　　沒去過的朋友可能不清楚，事實上澎湖花火節的花火不是天天施放的，往年都是每週一與每週四會施放煙火，雖然今年的詳細內容目前還沒出爐，但2017年的花火節已經升格，除了台灣自己的煙火團隊外，也有來自國際的法國煙火團隊，兩隊輪流施放，也可想而知今年必將更盛大。
+                            <img src="img/post/post2-6.jpg" width="50%">
+                            <br>　　所以如果是週休二日的普通上班族，最佳的前往時間是週六出發、週二回來的四天三夜行程，只需要請假兩天，就可以安穩的參加花火節，或是週三出發、週六回來，這樣還可以多休息一天準備上班。當然也可以依照班表選擇時間，但要注意的是小編通常建議不要將禮拜一與禮拜四排定在出發或回來的時間，否則有很大的機會跟花火擦身而過、或是根本沒體力好好享受花火。
+                        </font>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card-body" style="background-color:transparent;text-align:center">
+                        <font color="#000000" size="6" face="標楷體">花火回憶錄</font>
+                        <img src="img/post/post2-1.jpg" width="100%">
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card-body" style="background-color:transparent">
+                        <img src="img/post/post2-2.jpg" width="100%">
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card-body" style="background-color:transparent">
+                        <img src="img/post/post2-3.jpg" width="100%">
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card-body" style="background-color:transparent">
+                        <img src="img/post/post2-4.jpg" width="100%">
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card-body" style="background-color:transparent">
+                        <img src="img/post/post2-7.jpg" width="100%">
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card-body" style="background-color:transparent">
+                        <img src="img/post/post2-8.jpg" width="100%">
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card-body" style="background-color:transparent">
+                        <img src="img/post/post2-9.jpg" width="100%">
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card-body" style="background-color:transparent">
+                        <img src="img/post/post2-10.jpg" width="100%">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Pager -->
+        <ul class="pager">
+            <li class="next">
+                <a href="{{url('/post1')}}">Next Posts &rarr;</a>
+            </li>
+        </ul>
+    </div>
+@endsection
